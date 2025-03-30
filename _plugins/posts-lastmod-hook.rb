8684be0ab78194521a111904629dd81581b41c1a
wuzhi09 <kelaweita@gmail.com>
2025-03-31 02:19:09 +0800

    1st document
    using remove_dotdot.rb to avoid reading photoes issue between typora and jekyll.

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,13 @@
 #!/usr/bin/env ruby
 #
-# Check for changed posts
+# 多功能文章处理器：
+# 1. 记录文章最后修改时间
 
 Jekyll::Hooks.register :posts, :post_init do |post|
-
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
-
+  # 功能1：保留原有的最后修改时间记录
+  commit_num = `git rev-list --count HEAD "#{post.path}"`
   if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{post.path}"`
     post.data['last_modified_at'] = lastmod_date
   end
-
 end
---
+++ _plugins/remove_dotdot_assets.rb
@@ -0,0 +1,16 @@
+Jekyll::Hooks.register :site, :post_render do |page|
+  # 确保只对 Markdown 文件进行处理
+  if page.is_a?(Jekyll::Document) && page.extname == '.md'
+    # 读取当前 Markdown 文件的内容
+    content = page.content
+
+    # 只在文件中包含图片语法时进行处理
+    if content.include?("![") && content.include?("../")
+      # 替换路径，将 '../assets' 改为 '/assets'
+      updated_content = content.gsub(/\!\[([^\]]*)\]\(\.\.\/assets\//, '![\1](/assets/')
+
+      # 更新页面内容
+      page.content = updated_content
+    end
+  end
+end
